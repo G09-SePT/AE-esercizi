193824eb7ef5c441a59716209a2bdf6b69aadd95
Lorenzo Ferron <lorenzo_ferron@hotmail.it>
2018-06-11 09:25:12 +0200

    Rimosso newline superfluo

--- gedit_lang_def/gedit_lang_def.sh
+++ gedit_lang_def/gedit_lang_def.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.local/share/gtksourceview-3.0/language-specs
 mkdir -p ~/.local/share/gtksourceview-3.0/styles/
 cp ./jvm.lang ~/.local/share/gtksourceview-3.0/language-specs/
 cp ./classic.xml ~/.local/share/gtksourceview-3.0/styles/
-echo ""
 echo "This script overrides the default classic.xml for current user. To improve it, change it from ~/.local/share/gtksourceview-3.0/styles/classic.xml"
 return 0
 

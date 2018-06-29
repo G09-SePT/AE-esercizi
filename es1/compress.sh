a7c56b1be2ece18be69758eef718900feef32414
lorenzo <lorenzo_ferron@hotmail.it>
2018-06-29 21:30:00 +0200

    Aggiunte correzioni varie

--- es1/compress.sh
+++ es1/compress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zip -v -T -FS -r "es1_GR09" jas/ mal/ test_es1_mal/ ijvm.conf -x *.mic1 *.ijvm *.mne *.dbg CMakeLists.txt \*/.idea/\* \*/cmake-build-debug/\*
+zip -v -T -FS -r "es1_GR09" jas/ mal/ test_es1_mal/ ijvm.conf RelazioneEs1.pdf -x *.mic1 *.ijvm *.mne *.dbg CMakeLists.txt \*/.idea/\* \*/cmake-build-debug/\*
 
--- es2/compress.sh
+++ es2/compress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zip -v -T -FS -r "es2_GR09" jas/ mal/ gen_code/ src/ ijvm.conf EsempioTestSvolti.txt -x *.mic1 *.ijvm *.mne *.dbg CMakeLists.txt \*/.idea/\* \*/cmake-build-debug/\*
+zip -v -T -FS -r "es2_GR09" jas/ mal/ gen_code/ src/ ijvm.conf EsempioTestSvolti.txt RelazioneEs2.pdf -x *.mic1 *.ijvm *.mne *.dbg CMakeLists.txt \*/.idea/\* \*/cmake-build-debug/\*
 
